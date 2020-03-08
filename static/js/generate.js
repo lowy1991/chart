1cefefd1e62cb12f192eb86beea607019d4dce94
Lionel <lionelowy@hotmail.com>
2020-03-08 15:46:57 +0800

    Scaling

--- static/js/generate.js
+++ static/js/generate.js
@@ -1,10 +1,28 @@
 var play		= $('.carousel');
 var carousel 	= $('#carousel-inner');
+var icon 		= $('img');
 var indicator	= $('#carousel-indicator');
-var count 		= 0; 
+var count 		= 0;
 var page 		= 0;
+var maxCol 			= 10;
+var maxColMobile	= 5;
+var maxNum			= 0;
+
+function getWidth() {
+	var carouselItem= $('.carousel-item');
+	var imgWidth 	= 0; 
+	if (window.matchMedia("(max-width: 760px)").matches) {
+		maxNum		= 20;
+		return imgWidth	= Math.floor(parseInt(carouselItem.width())/maxColMobile);
+	}
+	else {
+		maxNum		= 50;
+		return imgWidth	= Math.floor(parseInt(carouselItem.width())/maxCol);	
+	}
+}
 
 function loadCarousel() {
+	var	imgWidth 	= 0;
 	for (i = 0; i < 10; i++) {
 		if (i == 0) {
 			carousel.append(`<div class="active carousel-item m-0 w-50"><div class="d-flex flex-wrap justify-content-center" id="chart-${i}"></div></div>`);
@@ -14,14 +32,18 @@ function loadCarousel() {
 			carousel.append(`<div class="carousel-item m-0 w-50"><div class="d-flex flex-wrap justify-content-center" id="chart-${i}"></div></div>`);
 			indicator.append(`<li data-target="#charts" data-slide-to="${i}"></li>`);
 		}
-		loadChart(i);
+		imgWidth = getWidth();
+		loadChart(i, imgWidth);
 	}
 }
 
-function loadChart(selector) {
-	for (j = 0; j < 36; j++) {
-  		$(`#chart-${selector}`).append('<img src="static/img/lion.svg" class="center-block filter p-2" id="lion-' + j + '" />');
+function loadChart(selector, imgWidth) {
+	var padding	= imgWidth/10;
+	for (j = 0; j < maxNum; j++) {
+  		$(`#chart-${selector}`).append(`<img src="static/img/lion.svg" class="center-block filter" id="lion-${j}"/>`);
   		$(`#chart-${selector} #lion-${j}`).css('filter', 'grayscale(100%)');
+  		$(`#chart-${selector} #lion-${j}`).css('padding', padding);
+  		$(`#chart-${selector} #lion-${j}`).width(imgWidth-padding*2);
 	}
 }
 
@@ -31,11 +53,10 @@ $('body').click(function() {
 
 $(document).ready(function() {
 	play.carousel('pause');
-	loadChart(0);
 	loadCarousel();
-	console.log($('img').css('width'));
+	console.log($('img').width());
 	console.log($('.carousel-item').css('width'));
-	console.log(parseInt($('.carousel-item').css('width'))/parseInt($('img').css('width')))
+	console.log(parseInt($('.carousel-item').css('width'))/parseInt($('img').css('width')));
 	// loadChart();
 	// $('#lion-'+count).css('cursor', 'pointer');
 	// $('#lion-'+count).addClass('filter');
