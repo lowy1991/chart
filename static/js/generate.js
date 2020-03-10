fbce323006d49d2a5f4a090a64fa484504fc0ba8
lowy1991 <pi@pi.openair4G.eur>
2020-03-10 14:33:08 +0800

    Date

--- static/js/generate.js
+++ static/js/generate.js
@@ -1,14 +1,17 @@
 var carousel		= $('.carousel');
 var carouselInner 	= $('#carousel-inner');
 var currPage		= $('#current-page');
+var month 			= $('#month');
 var totalPage 		= $('#total-page');
+var week 			= $('#week');
+var year 			= $('#year');
 var carouselPage	= 1;
 var count 			= 0;
 var countWeek		= 0;
 var countMonth		= 0; 
 var countYear		= 0;
 var page 			= 0;
-var	lastLogin		= 0;
+var	lastLogin		= new Date();
 var maxCol 			= 10;
 var maxColMobile	= 5;
 var maxItem			= 1000;
@@ -30,14 +33,13 @@ function change() {
 
 function checkCookie() {
 	var currCount 	= getCookie('count');
-	var d 			= new Date();
 	if (currCount == "") {
-		setCookie('count', 0, 365);
-		setCookie('countWeek', 0, 365);
-		setCookie('countMonth', 0, 365);
-		setCookie('countYear', 0, 365);
-		setCookie('lastLogin', d.toUTCString(), 365);
-		setCookie('page', 0, 365);
+		setCookie('count', count, 365);
+		setCookie('countWeek', countWeek, 365);
+		setCookie('countMonth', countMonth, 365);
+		setCookie('countYear', countYear, 365);
+		setCookie('lastLogin', lastLogin, 365);
+		setCookie('page', page, 365);
 	}
 	else {
 		count 		= parseInt(currCount);
@@ -77,6 +79,14 @@ function getMaxpages() {
 	totalPage.text(maxPage);
 }
 
+function getWeekNumber(d) {
+	d 	= new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+	d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+	var yearStart 	= new Date(Date.UTC(d.getUTCFullYear(),0,1));
+	var weekNo 		= Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+	return weekNo;
+}
+
 function getWidth() {
 	var carouselItem= $('.carousel-item');
 	var imgWidth 	= 0; 
@@ -113,12 +123,31 @@ function loadChart(selector, imgWidth) {
   		$(`#chart-${selector} #lion-${j}`).css('padding', padding);
   		$(`#chart-${selector} #lion-${j}`).width(imgWidth-padding*2);
 	}
-	readyChart();
 }
 
 function readyChart() {
 	$(`#chart-${page} #lion-${count}`).css('cursor', 'pointer');
 	$(`#chart-${page} #lion-${count}`).on('click', change);
+}
+
+function resetCount() {
+	var d = new Date();
+	if (d.getFullYear() > lastLogin.getFullYear()) {
+		countWeek 	= 0;
+		countMonth 	= 0;
+		countYear	= 0;
+	}
+	else {
+		if (d.getMonth() > lastLogin.getMonth()) {
+			countMonth	= 0;
+			countWeek	= 0;
+		}
+		else {
+			if (getWeekNumber(d) > getWeekNumber(lastLogin)) {
+				countWeek	= 0;
+			}
+		}
+	}
 }
 
 function setCookie(cname, cvalue, exdays) {
@@ -130,6 +159,7 @@ function setCookie(cname, cvalue, exdays) {
 }
 
 function update() {
+	var d 			= new Date();
 	if (count == (maxNum-1)) {
 		count 	= 0;
 		page++;
@@ -138,8 +168,23 @@ function update() {
 	else {
 		count 	= (count+1)%maxNum;
 	}
+	countWeek++;
+	countMonth++;
+	countYear++;
+	updateCounter();
+
 	setCookie('count', count, 365);
+	setCookie('countWeek', countWeek, 365);
+	setCookie('countMonth', countMonth, 365);
+	setCookie('countYear', countYear, 365);	
+	setCookie('lastLogin', d.toUTCString(), 365);
 	setCookie('page', page, 365);
+}
+
+function updateCounter() {
+	week.text(countWeek*15);
+	month.text(countMonth*0.25);
+	year.text(countYear*0.25);
 }
 
 carousel.bind('slide.bs.carousel', function (e) {
@@ -164,5 +209,7 @@ carousel.bind('slide.bs.carousel', function (e) {
 
 $(document).ready(function() {
 	checkCookie();
+	resetCount();
 	loadCarousel();
+	readyChart();
 });
